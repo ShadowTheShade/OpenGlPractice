4569d6ddf5558bf7782769e7d2960c58b2eba494
Anthony Gagliano <antogagliano3@gmail.com>
2023-08-06 17:45:56 -0400

    Removed org document. Sandbox axis.

--- Sandbox/src/sandbox.cpp
+++ Sandbox/src/sandbox.cpp
@@ -246,7 +246,10 @@ int main()
 		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		//Rebind VAO
 		cubeShader.use();
+		
+		glBindVertexArray(cubeVAO);
 		cubeShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
 		cubeShader.setVec3("lightPos", lightPos);
 		cubeShader.setVec3("viewPos", camera.Position);
@@ -265,13 +268,17 @@ int main()
 		//glActiveTexture(GL_TEXTURE0);
 		//glBindTexture(GL_TEXTURE_2D, texture1);
 
-		//Rebind VAO
+
 
 		// X-axis
-	    		
-		glBindVertexArray(cubeVAO);
 		
-		Shapes::makeCube(cubeShader, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f), glm::vec3(100.0f, 1.0f, 1.0f), 0.0f);
+
+		cubeShader.setVec3("objectColor", 1.0f, 0.0f, 0.0f);
+		Shapes::makeCube(cubeShader, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f), glm::vec3(100.0f, 0.5f, 0.5f), 0.0f);
+		cubeShader.setVec3("objectColor", 0.0f, 1.0f, 0.0f);
+		Shapes::makeCube(cubeShader, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f), glm::vec3(0.5f, 100.0f, 0.5f), 0.0f);
+		cubeShader.setVec3("objectColor", 0.0f, 0.0f, 1.0f);
+		Shapes::makeCube(cubeShader, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f), glm::vec3(0.5f, 0.5f, 100.0f), 0.0f);
 
 
 		//Draw
